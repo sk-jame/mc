297b91d968d47142f06e6e724c7b52e922e01536
Andrew Borodin <aborodin@vmail.ru>
2017-06-04 10:10:39 +0300

    mcview: minor refactoring.
    * (mcview_growbuf_read_all_data): new function.
    
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- src/viewer/inlines.h
+++ src/viewer/inlines.h
@@ -1,7 +1,12 @@
 #ifndef MC__VIEWER_INLINES_H
 #define MC__VIEWER_INLINES_H
 
+#include <limits.h>             /* CHAR_BIT */
+
 /*** typedefs(not structures) and defined constants **********************************************/
+
+#define OFF_T_BITWIDTH  ((unsigned int) (sizeof (off_t) * CHAR_BIT - 1))
+#define OFFSETTYPE_MAX (((off_t) 1 << (OFF_T_BITWIDTH - 1)) - 1)
 
 /*** enums ***************************************************************************************/
 
@@ -156,5 +161,11 @@ mcview_is_nroff_sequence (WView * view, off_t offset)
 }
 
 /* --------------------------------------------------------------------------------------------- */
+
+static inline void
+mcview_growbuf_read_all_data (WView * view)
+{
+    mcview_growbuf_read_until (view, OFFSETTYPE_MAX);
+}
 
 #endif /* MC__VIEWER_INLINES_H */
